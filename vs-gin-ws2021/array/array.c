24290efb4a68f1c9c8233d95af12bff428ffb256
Simon Bekemeier <simon.bekemeier@fh-bielefeld.de>
2020-10-24 19:52:18 +0200

    Replace literal by constant

--- vs-gin-ws2021/array/array.c
+++ vs-gin-ws2021/array/array.c
@@ -1,16 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+#define ARRAY_SIZE 5
+
 int main(int argc, char **argv) {
-    int zahlen[5];
+    int zahlen[ARRAY_SIZE];
     int i;
 
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < ARRAY_SIZE; i++) {
         printf("Eingabe %d: ", i);
         scanf("%d", &zahlen[i]);
     }
-    
-    for (i = 0; i < 5; i++) {
+
+    for (i = 0; i < ARRAY_SIZE; i++) {
         printf("Doppelt: %d\n", zahlen[i] * 2);
     }
 }
